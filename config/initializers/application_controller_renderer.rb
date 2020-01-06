51e28b04137ed139efa30f3652753953a2f96fed
そそ <sora@sosonoMacBook-Air.local>
2020-01-06 12:53:57 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7fd38f043c1ffacc5c073f7707f54632cad08a094c02f91f5edfccba7d987d3f59c545cdfe1b649e53119e1daa9026c3061178a388b69fa0c54bd3b53b41b347'
